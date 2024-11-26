1b277f801603b7ff8857ef3cec61dc5d0b5d7466
Neel Pandey <neelpandeyofficial@gmail.com>
2024-11-26 15:05:37 +0530

    Update adding_multidimensional_lists.py

--- adding_multidimensional_lists.py
+++ adding_multidimensional_lists.py
@@ -1,7 +1,9 @@
 #adding multiple multidimensional lists
+#take two matrices of your choices 
+# let the third matrix be empty
 l4=[[1,2,3],[4,5,6],[7,8,9]]
 l5=[[9,8,7],[6,5,4],[3,2,1]]
-l6=[[0,0,0],[1,1,1],[2,2,2]]
+l6=[[0,0,0],[0,0,0],[0,0,0]]
 
 for i in range(0, 3):
 	for j in range(0, 3):
